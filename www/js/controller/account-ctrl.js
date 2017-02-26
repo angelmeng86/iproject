63edc884ac775d661df20bbd3296935a4eba7910
Maple <angelmeng86@163.com>
2017-02-26 10:46:31 +0800

    1、对工程开发目录结构进行调整

---
+++ www/js/controller/account-ctrl.js
@@ -0,0 +1,7 @@
+angular.module('accountctrl', [])
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
---
+++ www/js/controller/dash-ctrl.js
@@ -0,0 +1,3 @@
+angular.module('dashctrl', [])
+
+.controller('DashCtrl', function($scope) {});
